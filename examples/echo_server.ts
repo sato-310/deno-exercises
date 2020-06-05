e905388513a8193834bc9fd8e81653ae06ad741a
sato-310 <kikyo.310.906@gmail.com>
2020-06-06 00:05:57 +0900

    comment

--- examples/echo_server.ts
+++ examples/echo_server.ts
@@ -1,3 +1,5 @@
+// https://deno.land/manual/examples/tcp_echo
+// $ deno run --allow-net echo_server.ts
 const listener = Deno.listen({ port: 8080 });
 console.log("listening on 0.0.0.0:8080");
 for await (const conn of listener) {
